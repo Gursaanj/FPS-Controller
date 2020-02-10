a940ffb4263a3340fe10ad9a1c6f060f0b216bec
Gursaanj <gursaanjresides@gmail.com>
2020-02-10 01:52:23 -0800

    Movement controller

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity;
+    [SerializeField] private Transform playerBody;
+    private float mouseX = 0f;
+    private float mouseY = 0f;
+    private float xRotation = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    private void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        RotateBody();
+    }
+
+    private void RotateBody()
+    {
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private float gravity = -9.81f;
+    private CharacterController controller;
+    private float horizontalAxis;
+    private float verticalAxis;
+    private float minSlopeLimit = 45.0f;
+    private float maxSlopeLimit = 100.0f;
+    private Vector3 horizontalMovement;
+    private Vector3 verticalMovement;
+    private Vector3 movement;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float groundedVelocity = -2f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        controller = GetComponentInChildren<CharacterController>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+
+        Move();
+        VerticalMotion();
+        Jump();
+    }
+
+    private void Move()
+    {
+        horizontalMovement = transform.right * horizontalAxis;
+        verticalMovement = transform.forward * verticalAxis;
+        movement = Vector3.Normalize(horizontalMovement + verticalMovement);
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+
+    private void VerticalMotion()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(0.5f * velocity * Time.deltaTime);
+        ResetVerticalVelocity();
+    }
+
+    private void ResetVerticalVelocity()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            //to ensure that the player is forced down on the ground
+            controller.slopeLimit = minSlopeLimit;
+            velocity.y = groundedVelocity;
+        }
+
+        if ((controller.collisionFlags & CollisionFlags.Above) != 0)
+        {
+            velocity.y = groundedVelocity;
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            controller.slopeLimit = maxSlopeLimit;
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+}
